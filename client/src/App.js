4a758a1b6f1efb73dedcb5d64264d20ed2f4da10
Rahul Dahal <rdaahal@gmail.com>
2020-09-26 10:00:48 +0545

    ready to start development process...

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Customers from './components/customers';
 
 class App extends Component {
   render() {
@@ -9,9 +8,8 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">React Express Starter</h1>
+          <h1 className="App-title">My Playbook</h1>
         </header>
-        <Customers />
       </div>
     );
   }
