2706c34f7ec8c41639c03f749fe17d1d0f07f2e7
Rahul Dahal <rdaahal@gmail.com>
2020-10-02 14:35:04 +0545

    separated 'mongoose connection' from server.js, begin listening for requests only after the connection is established

---
+++ db.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const app = require("./server");
+
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => afterConnection())
+  .catch((err) => console.error("Cannot connect to MongoDB"));
+
+function afterConnection() {
+  console.log("Connected to MongoDB");
+  // session setup
+  let sessionOptions = {
+    secret: process.env.SESSION_SECRET,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 1000 * 60 * 60 * 24, httpOnly: true },
+  };
+
+  app.use(session(sessionOptions));
+
+  // begin listening
+  const port = process.env.PORT || 5000;
+
+  app.listen(port, () => console.log(`Server running on port ${port}`));
+}
--- server.js
+++ server.js
@@ -1,37 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const session = require("express-session");
 const passport = require("passport");
-const MongoStore = require("connect-mongo")(session);
 const helmet = require("helmet");
 const passportController = require("./controllers/passport");
 const dotenv = require("dotenv");
 dotenv.config();
+const path = require("path");
 
 const app = express();
 
 app.use(helmet());
-
-// connecting to MongoDB via mongoose
-mongoose
-  .connect(process.env.MONGODB_CONNECTION_STRING, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.error("Cannot connect to MongoDB"));
-
-// session setup
-
-let sessionOptions = {
-  secret: process.env.SESSION_SECRET,
-  store: new MongoStore({ mongooseConnection: mongoose.connection }),
-  resave: false,
-  saveUninitialized: false,
-  cookie: { maxAge: 1000 * 60 * 60 * 24, httpOnly: true },
-};
-
-app.use(session(sessionOptions));
 
 // initialize passportJS and use express-session
 app.use(passport.initialize());
@@ -44,6 +22,4 @@ app.use("/*", (req, res) =>
   res.status(404).json({ message: "The requested route is not found!" })
 );
 
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => console.log(`Server running on port ${port}`));
+module.exports = app;
